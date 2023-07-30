e6f1c55d5f2e289f493ea09e8cf829b1535726b6
jaehoon <rival0605@gmail.com>
2023-07-30 10:51:37 +0900

    fix font size 12 to 10 on time_row

--- lib/src/widgets/time_row.dart
+++ lib/src/widgets/time_row.dart
@@ -46,13 +46,11 @@ class _TimeRowState extends State<TimeRow> {
     int candleNumber = (step + 1) ~/ 2 - 10 + index * step + -1;
     DateTime? _time;
     if (candleNumber < 0)
-      _time = widget.candles[0].date.add(Duration(
-          milliseconds: dif.inMilliseconds ~/ -1 * step * candleNumber));
+      _time = widget.candles[0].date.add(Duration(milliseconds: dif.inMilliseconds ~/ -1 * step * candleNumber));
     else if (candleNumber < widget.candles.length)
       _time = widget.candles[candleNumber].date;
     else {
-      _time = widget.candles[0].date.subtract(
-          Duration(milliseconds: dif.inMilliseconds ~/ step * candleNumber));
+      _time = widget.candles[0].date.subtract(Duration(milliseconds: dif.inMilliseconds ~/ step * candleNumber));
     }
     return _time;
   }
@@ -90,8 +88,7 @@ class _TimeRowState extends State<TimeRow> {
 
   @override
   void didUpdateWidget(TimeRow oldWidget) {
-    if (oldWidget.index != widget.index ||
-        oldWidget.candleWidth != widget.candleWidth)
+    if (oldWidget.index != widget.index || oldWidget.candleWidth != widget.candleWidth)
       _scrollController.jumpTo((widget.index + 10) * widget.candleWidth);
     super.didUpdateWidget(oldWidget);
   }
@@ -99,8 +96,7 @@ class _TimeRowState extends State<TimeRow> {
   @override
   Widget build(BuildContext context) {
     int step = _stepCalculator();
-    final dif =
-        widget.candles[0].date.difference(widget.candles[1].date) * step;
+    final dif = widget.candles[0].date.difference(widget.candles[1].date) * step;
     return Padding(
       padding: const EdgeInsets.only(right: PRICE_BAR_WIDTH + 1.0),
       child: Stack(
@@ -142,7 +138,7 @@ class _TimeRowState extends State<TimeRow> {
                         dateFormatter(widget.indicatorTime!),
                         style: TextStyle(
                           color: widget.style.secondaryTextColor,
-                          fontSize: 12,
+                          fontSize: 10,
                         ),
                       ),
                     ),
